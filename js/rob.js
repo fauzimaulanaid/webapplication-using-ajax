30a4468dab71e9e702d7c7bdbc8278809f1edc8f
fauzimaulana15 <muhammadfauzi1933@gmail.com>
2020-12-20 00:59:38 +0700

    web ajax

---
+++ js/rob.js
@@ -0,0 +1,25 @@
+//the document ready will be trigerred when all component in the web page has been displayed
+$(document).ready(function () {
+    //display beranda in the first time
+    $.ajax({
+        url: "beranda.html",
+        success: (result) => {
+            $("#div1").html(result);
+        },
+        error: (err) => {
+            $("#div1").html(err.responseText);
+        }
+    });
+    
+    //the a click will be trigerred if an a-tagged is clicked
+    $("a").click(function (e) {
+        var data = $(e.currentTarget).data("page");
+        //Reding data from the server directly and return the result in a call back function
+        $.ajax({url: data, success: function(result){
+            $("#div1").html(result);
+        },error: function(xhr,status,err){
+            $("#div1").html("<h2>" + xhr.status + ":" + err + "<h2>");
+        }
+    });
+    });
+});
